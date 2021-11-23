478bec405739284fc68ee537d4297b970eb31a01
Javad Meghrazi <meghrazijavad@gmail.com>
2021-11-23 00:41:49 -0800

    latest version of summary latest version of code and shiny app first version of lessons from kefir

--- Model4_code.R
+++ Model4_code.R
@@ -1,17 +1,27 @@
-#packages
-#install.packages("expm")
-#library (expm)
+#functions
+matrix_exp <- function(mat,n){
+  if(!is.matrix(mat) | (nrow(mat)!=ncol(mat)) | n <0){
+    return(0)
+  }
+  if(n == 0){
+    return(diag(nrow(mat)))
+  }
+  matnew <- diag(nrow(mat))
+  for(i in 1:n){
+    matnew <- matnew %*% mat
+  }
+  matnew
+}
 #parameters
 gl = 101
 wa = 1
 gl_0 = 51
 gr = 0
+s_rounds = 20
 mu = 0
 mi = 0
 gen = 1000
 
-#t = seq(0, gen, length.out = 1000)
-# normal growth rate of grains is assumed to be 2
 gr_0 = 1
 mass = matrix (0, gl, gen+1)
 freq = matrix (0, gl, gen+1)
@@ -29,31 +39,41 @@ freq [gl_0,1] = 100
 # calculating the frequencies and average fitness in each grain level
 
 allele_f = seq (0,1,1/(gl-1))
+allele_f_after_s = allele_f[]*wa/(allele_f[]*wa+(1-allele_f[]))
 mean_allele_f[1]=allele_f[gl_0] 
 # effect of group growth changes
 for (i in 1:gl) {
   Growth_mat [i,i] = gr_0 + (i-1)/(gl-1)*gr
 }
 # effect of individual frequency changes in each grain due to natural selection
-
-for (i in 1:gl){
-  add_fit [i] = (allele_f[i]*wa/(allele_f[i]*wa+1-allele_f[i]))-allele_f[i]
-  j = add_fit[i]%/%(1/(gl-1))
-  k = add_fit[i]%%(1/(gl-1))
-  if (add_fit[i]<0){
-    if (i>-j){
-      selection_mat [i+j,i]= 1-(k/(1/(gl-1))) 
-    }
-    selection_mat [i+j+1,i]= k/(1/(gl-1))
-  } else {
-    if (i<(gl-j)){
-      selection_mat [i+j+1,i]= k/(1/(gl-1))
-    }
-    selection_mat [i+j,i]= 1-(k/(1/(gl-1)))
+#version 1
+#for (i in 1:gl){
+#  add_fit [i] = (allele_f[i]*wa/(allele_f[i]*wa+1-allele_f[i]))-allele_f[i]
+#  j = add_fit[i]%/%(1/(gl-1))
+#  k = add_fit[i]%%(1/(gl-1))
+#  if (add_fit[i]<0){
+#    if (i>-j){
+#      selection_mat [i+j,i]= 1-(k/(1/(gl-1))) 
+#    }
+#    selection_mat [i+j+1,i]= k/(1/(gl-1))
+#  } else {
+#    if (i<(gl-j)){
+#      selection_mat [i+j+1,i]= k/(1/(gl-1))
+#    }
+#    selection_mat [i+j,i]= 1-(k/(1/(gl-1)))
+#  }
+#}
+#version_2 (birth_death model)
+for (j in 1:gl){
+  selection_mat [j,j] = allele_f[j]*allele_f_after_s[j]+(1-allele_f[j])*(1- allele_f_after_s[j])
+  if (j+1 <= gl){
+    selection_mat [j+1,j] = (1-allele_f[j])*allele_f_after_s[j]
   }
+  if (j-1 >= 1){
+    selection_mat [j-1,j] = allele_f[j]*(1-allele_f_after_s[j])
+    }
 }
-
-
+selection_mat_final = matrix_exp(selection_mat,s_rounds)
 # effect of drift_version_1
 #for (i in 2:(gl-1)){
 #  drift_effect [i] = allele_f[i]*(1-allele_f[i])/n
@@ -82,6 +102,8 @@ for (i in 1:gl){
 
 
 # effect of mutation (in both directions)
+drift_mut_mat = diag (gl)
+#the previous line is included because birth/death model has inherent drift
 drift_mut_mat [2,1] = mu
 drift_mut_mat [1,1] = 1-mu
 drift_mut_mat [gl-1,gl] = mu
@@ -103,7 +125,7 @@ for (i in 2:gen){
   for (k in 1:mi){
     mig_mat_final = mig_mat_final %*% mig_mat
   }
-  mass [,i]= Growth_mat %*% selection_mat %*% drift_mut_mat %*% mig_mat_final %*% mass [,i-1]
+  mass [,i]= Growth_mat %*% selection_mat_final %*% drift_mut_mat %*% (matrix_exp(mig_mat,mi)) %*% mass [,i-1]
   freq [,i]= 100* mass [,i]/ sum(mass[,i])
   mean_allele_f[i] = sum (freq[,i]*allele_f[])/gl
 
